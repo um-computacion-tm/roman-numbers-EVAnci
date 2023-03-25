e4a28543101653d4ea4caa983b8c8049db8e13c2
EVAnci <e.anci@alumno.um.edu.ar>
2023-03-25 13:38:29 -0300

    Only work with simple numbers

--- main.py
+++ main.py
@@ -2,22 +2,54 @@
 #   Function    #
 #################
 
-def roman_basic(number):
-    num = {
+def decimal_to_roman(number):
+    de2ro = {
         '1' : 'I',
         '4' : 'IV',
         '5' : 'V',
         '9' : 'IX',
         '10' : 'X',
+        '40' : 'XL',
         '50' : 'L',
+        '90' : 'XC',
         '100' : 'C',
+        '400' : 'CD',
         '500' : 'D',
+        '900' : 'CM',
         '1000' : 'M'
-    }
-    return num[str(number)]
+        }
+    decimal = ['1',
+               '4',
+               '5',
+               '9',
+               '10',
+               '40',
+               '50',
+               '90',
+               '100',
+               '400',
+               '500',
+               '900',
+               '1000'
+               ]
+    
+    for i in range(len(decimal)):
 
-def decimal_to_roman(decimal):
-    if roman_basic(decimal) == None:
-        print('not a basic number')
+        if number == int(decimal[i]):
+            print(f'number in: {number}, decimal in: {decimal[i]}')
+            roman = de2ro[decimal[i]]
+            number = 0
+            break
+        elif number < int(decimal[i]):
+            print(f'number in: {number}, decimal in: {decimal[i]}')
+            roman = de2ro[decimal[i-1]]
+            number = number - int(decimal[i-1])
+            break
+    print(f'number return: {number}, roman return: {roman}')
+    if number == 0:
+        return roman
     else:
-        return roman_basic(decimal)
+        roman = roman + decimal_to_roman(number)
+
+
+print(decimal_to_roman(3))
--- test_main.py
+++ test_main.py
@@ -16,7 +16,7 @@ class TestDecimalToRoman(unittest.TestCase):
         decimal_numbers = [1,5,10,50,100,500,1000]
         # Test numbers in the list 
         for i in range(len(roman_numbers)):
-            #print(f'Test: in[{decimal_numbers[i]}] --> out[{roman_numbers[i]}]')
+            print(f'Test: in[{decimal_numbers[i]}] --> out[{roman_numbers[i]}]')
             resultado = decimal_to_roman(decimal_numbers[i])
             self.assertEqual(resultado, roman_numbers[i])
 
