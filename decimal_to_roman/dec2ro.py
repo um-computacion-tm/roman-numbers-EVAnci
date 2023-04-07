9d28f0e423b7adced8d8ce2b61034748ac488010
EVAnci <e.anci@alumno.um.edu.ar>
2023-04-06 22:47:35 -0300

    Functionality improved

---
+++ decimal_to_roman/dec2ro.py
@@ -0,0 +1,86 @@
+#################
+#   Function    #
+#################
+
+def decimal_to_roman(number):
+    de2ro = {
+        '1' : 'I',
+        '4' : 'IV',
+        '5' : 'V',
+        '9' : 'IX',
+        '10' : 'X',
+        '40' : 'XL',
+        '50' : 'L',
+        '90' : 'XC',
+        '100' : 'C',
+        '400' : 'CD',
+        '500' : 'D',
+        '900' : 'CM',
+        '1000' : 'M',
+        '4000' : 'MV̅',
+        '5000' : 'V̅',
+        '9000' : 'MX̅',
+        '10000' : 'X̅',
+        '40000' : 'X̅L̅',
+        '50000' : 'L̅',
+        '90000' : 'X̅C̅',
+        '100000' : 'C̅',
+        '400000' : 'C̅D̅',
+        '500000' : 'D̅',
+        '900000' : 'C̅M̅',
+        '1000000' : 'M̅'
+        }
+    decimal = ['1',
+               '4',
+               '5',
+               '9',
+               '10',
+               '40',
+               '50',
+               '90',
+               '100',
+               '400',
+               '500',
+               '900',
+               '1000',
+               '4000',
+               '5000',
+               '9000',
+               '10000'
+               ]
+
+    # How it works?
+    # if input number is equal to a number in the list, it's assigned
+    # if not, it divide the number in 2 parts...
+    # the first part, that consist in the bigger part of the number
+    # example: 48 -> first part 40, second part 8
+    #  the 8 then -> first part 5, second part 3, and so
+    # if the number is bigger than the last number in the list, that 
+    # number will be selected (bigger in the list)
+    # At the end recursivity join all number parts togeher
+
+    for i in range(len(decimal)):
+        if number == int(decimal[i]):
+            # print(f'number in: {number}, decimal in: {decimal[i]}')
+            roman = de2ro[decimal[i]]
+            number = 0
+            break
+        elif number < int(decimal[i]):
+            # print(f'number in: {number}, decimal in: {decimal[i]}')
+            roman = de2ro[decimal[i-1]]
+            number = number - int(decimal[i-1])
+            break
+        elif number > int(decimal[-1]):
+            roman = de2ro[decimal[-1]]
+            number = number - int(decimal[-1])
+            break
+    # print(f'number return: {number}, roman return: {roman}')
+    if number == 0:
+        # print('Number returned successfully')
+        return roman
+    else:
+        # print('Recursivity...')
+        result = roman + decimal_to_roman(number)
+    return result
+
+# print(decimal_to_roman(int(input('Type a number: '))))
